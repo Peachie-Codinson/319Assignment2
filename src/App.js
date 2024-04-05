1f56d1d600ac3addf41eacc1ccab649559dc1d42
Peachie-Codinson <buinhatanh309@gmail.com>
2024-04-05 11:22:32 -0500

    sidebar ui done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import logo2 from './logo.png';
 import productData from './products.json';
 
+
 const Header = ({ onSearch, clearCart, toggleSidebar, isSidebarOpen }) => {
   const handleSearch = (event) => {
     if (event.key === 'Enter' || event.type === 'click') {
@@ -48,7 +49,54 @@ const Header = ({ onSearch, clearCart, toggleSidebar, isSidebarOpen }) => {
   );
 };
 
-const Sidebar = ({ isOpen, toggleSidebar, cartItems }) => {
+const CartItemCard = ({ productId, product, cartItems, handleAdd, handleSubtract, handleDelete }) => {
+  return (
+    <div key={productId} className="card mb-3 mt-3 me-2 ms-2">
+      <div className="container-fluid">
+        <div className="row g-0">
+          <div className="col-md-2 d-flex align-items-center justify-content-center">
+            <img
+              src={product.image}
+              className="card-img-top pt-2 pb-2"
+            />
+          </div>
+
+          <div className="col-md-7">
+            <div className="card-body">
+              <h5 className="card-title">{product.title}</h5>
+              <h5 className="card-title mt-4">Price: ${product.price}</h5>
+            </div>
+            
+          </div>
+
+          <div className='col-md-3'>
+            <div className='d-flex flex-column'>
+            <div className="d-flex align-items-end flex-column mt-4 me-3">
+              <div className="btn-group " role="group">
+                <button type="button" className="btn btn-secondary btn-md" onClick={() => handleAdd(productId)}>+</button>
+                <div className="container col-md-5 d-flex align-items-center justify-content-center border">
+                  <span className="text-center align-self-center">{cartItems[productId]}</span>
+                </div>
+                <button type="button" className="btn btn-secondary btn-md" onClick={() => handleSubtract(productId)}>-</button>
+              </div>
+            </div>
+            <div className="mt-4 mb-2 me-2 align-self-end">
+            <button type="button" className=" btn btn-secondary btn-md" onClick={() => handleDelete(productId)}>
+              <i className="bi bi-trash"></i>
+            </button>
+
+            </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+//temporary cart view
+const Sidebar = ({ isOpen, toggleSidebar, cartItems, handleAdd, handleSubtract, handleDelete, calculateTotal }) => {
   return (
     <>
       {isOpen && <div className="overlay"></div>}
@@ -56,41 +104,55 @@ const Sidebar = ({ isOpen, toggleSidebar, cartItems }) => {
 
         <div className="container d-flex flex-column vh-100">
 
-
           <hr className="hr hr-blurry bg-dark m-0 mt-3" />
 
-          <div className="d-flex justify-content-between">
-            <h1 className="text-left mt-3 mb-3 ms-2">
-              <i className="bi bi-cart me-4" style={{ fontSize: '2.5rem' }}></i>
-              Your Cart
+          <div className="d-flex justify-content-between align-content-center no-wrap">
+            <h1 className="text-left mt-3 mb-3 ms-2" style={{ whiteSpace: 'nowrap' }}>
+              <i className="bi bi-cart me-md-4 me-sm-3" style={{ fontSize: '2.5rem' }}></i>
+              <span className="d-none d-md-inline">Your Cart</span>
+              <span className="d-inline d-md-none" style={{ fontSize: '1.5rem' }}>Your Cart</span>
             </h1>
-            <button className="btn btn-outline-secondary align-self-center rounded-circle me-5" onClick={toggleSidebar}>
+
+            <button className="btn btn-outline-secondary align-self-center rounded-circle ms-md-3 me-sm-2 me-md-3 me-sm-2" onClick={toggleSidebar}>
               <i className="bi bi-x" style={{ fontSize: '1.3rem' }}></i>
             </button>
           </div>
 
           <hr className="hr hr-blurry bg-dark m-0" />
 
-          <div className="cart-items overflow-auto">
-            {Object.keys(cartItems).map(productId => (
-              <div key={productId} className="card mb-3">
-                <img src={cartItems[productId].image} className="card-img-top" alt={cartItems[productId].title} />
-                <div className="card-body">
-                  <h5 className="card-title">{cartItems[productId].title}</h5>
-                  <p className="card-text">Count: {cartItems[productId].count}</p>
-                </div>
-              </div>
-            ))}
+          <div className="cart-items overflow-auto  me-1 mt-2 mb-2">
+            {Object.keys(cartItems).map(productId => {
+              const product = productData.find(item => item.id === parseInt(productId, 10));
+              if (product) {
+                return (
+                  <CartItemCard
+                    key={productId}
+                    productId={productId}
+                    product={product}
+                    cartItems={cartItems}
+                    handleAdd={handleAdd}
+                    handleSubtract={handleSubtract}
+                    handleDelete = {handleDelete}
+                  />
+                );
+              } else {
+                return null;
+              }
+            })}
           </div>
-          <hr className="mt-auto hr hr-blurry  bg-dark m-0 mb-3" />
+
+          <hr className="mt-auto hr hr-blurry  bg-dark m-0" />
+          <div className='d-flex flex-row justify-content-between mt-2 mb-3'>
+              <h3 className="card-title ">Subtotal:</h3>
+              <h3 className="card-title me-2">${calculateTotal()}</h3>
+          </div>
+          
+          <hr className="hr hr-blurry  bg-dark m-0 mb-3" />
           <div className="mb-3 d-flex  justify-content-center">
-              
-              <button className="btn btn-primary btn-lg checkout-btn w-100 text-dark" style={{ backgroundColor: "#ff9900", borderColor: "#ff9900"}}>Proceed to Payment & Checkout</button>
+            <button className="btn btn-primary btn-lg checkout-btn w-100 text-dark" style={{ backgroundColor: "#ff9900", borderColor: "#ff9900" }}>Proceed to Payment & Checkout</button>
           </div>
 
         </div>
-
-
       </div>
     </>
   );
@@ -98,6 +160,8 @@ const Sidebar = ({ isOpen, toggleSidebar, cartItems }) => {
 
 
 
+
+//Search by filters, located udner header
 const FilterBar = ({ filterProducts }) => {
   const filterTitles = ["All", "Jewelery", "Men's clothing", "Women's clothing", "Electronics"];
 
@@ -123,6 +187,7 @@ const FilterBar = ({ filterProducts }) => {
   );
 };
 
+//Generate stars based on numerical rating
 const generateStarIcons = (rating) => {
   const stars = [];
   const filledStars = Math.floor(rating);
@@ -141,6 +206,7 @@ const generateStarIcons = (rating) => {
   return stars;
 };
 
+//Contains the body that hold all cards
 const ProductCard = ({ product, handleAdd, handleSubtract, cartItems }) => {
   return (
     <div className="col-sm-6 col-md-4 col-lg-3 mb-4">
@@ -157,6 +223,7 @@ const ProductCard = ({ product, handleAdd, handleSubtract, cartItems }) => {
   );
 };
 
+//Contains the card
 const ProductCardBody = ({ product, handleAdd, handleSubtract, cartItems }) => {
   return (
     <div className="card-body flex-fill d-flex flex-column ">
@@ -229,6 +296,27 @@ const App = () => {
     }
   };
 
+  const handleDelete = (productId) => {
+    setCartItems(prevState => {
+      const updatedCart = { ...prevState };
+      updatedCart[productId] = 0; // Set count to 0
+      delete updatedCart[productId]; // Remove item from cart
+      return updatedCart;
+    });
+  };
+
+  const calculateTotal = () => {
+    let total = 0;
+    Object.keys(cartItems).forEach(productId => {
+      const product = productData.find(item => item.id === parseInt(productId, 10));
+      if (product) {
+        total += product.price * cartItems[productId];
+      }
+    });
+    return total.toFixed(2);
+  };
+  
+
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
     if (isSidebarOpen) {
@@ -238,10 +326,23 @@ const App = () => {
     }
   };
 
+  
+
   return (
     <div>
       <Header onSearch={handleSearch} clearCart={clearCart} toggleSidebar={toggleSidebar} />
-      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} cartItems={cartItems} />
+
+      <Sidebar
+        isOpen={isSidebarOpen}
+        toggleSidebar={toggleSidebar}
+        cartItems={cartItems}
+        handleAdd={handleAdd}
+        handleSubtract={handleSubtract}
+        handleDelete = {handleDelete}        
+        calculateTotal={calculateTotal}
+      />
+
+
       <hr className="hr hr-blurry bg-dark m-0" />
       <FilterBar filterProducts={filterProducts} />
 
